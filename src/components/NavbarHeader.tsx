de35774e58ffdc9fe191cefc12e38743026e21f2
GrumpyEric <55453689+GrumpyEric@users.noreply.github.com>
2023-09-06 15:42:10 +0200

    fill image in div

--- src/components/NavbarHeader.tsx
+++ src/components/NavbarHeader.tsx
@@ -7,7 +7,7 @@ export default function NavbarHeader() {
 
   useEffect(() => {
     const listener = () => {
-      if (window.scrollY > 200) {
+      if (window.scrollY > 1) {
         setAnimateHeader(true)
       
       } else {
@@ -23,7 +23,7 @@ export default function NavbarHeader() {
   }, [])
 
   return (
-    <header className={`w-full z-40 bg-opacity-100 desktop:h-24 mobile:h-16 flex flex-row mobile:justify-center desktop:justify-between items-center desktop:px-20 mobile:px-1/16 sticky top-0 backdrop-filter backdrop-blur-lg transition ease-in-out duration-200 ${
+    <header id='header' className={`w-full z-40 bg-opacity-100 flex flex-row py-4 mobile:justify-center desktop:justify-between items-center desktop:px-20 mobile:px-1/16 sticky top-0 backdrop-filter backdrop-blur-lg transition ease-in-out duration-200 ${
       animateHeader 
         ? 'shadow-2xl bg-accent bg-opacity-50'
         : 'bg-accent'
