8abc27a4bc856ba179c6ebcd125444f2cbcabbc5
GrumpyEric <55453689+GrumpyEric@users.noreply.github.com>
2023-09-14 11:28:51 +0200

    deleted unneeded files

--- src/components/constants/enums/iconEnums.ts
+++ src/components/constants/enums/iconEnums.ts
@@ -1,7 +1,14 @@
-export enum socialsIconsEnums {
+export enum IconsEnums {
   Instagram = "FaInstagram",
   Youtube = "FaYoutube",
   Twitch = "FaTwitch",
   Twitter = "FaTwitter",
   Calendar = "FaCalendarPlus",
+}
+
+export interface IconProps {
+  icon: IconsEnums
+  size: number
+  animate: boolean,
+  className?: string
 }
