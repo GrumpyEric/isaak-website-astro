188c5eeb8df61882adb9d90fa98cf868412ce34b
GrumpyEric <55453689+GrumpyEric@users.noreply.github.com>
2024-04-09 19:09:37 +0200

    clean up astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,14 @@
 import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
-import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
-import netlifyIntegration from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  // adapter: netlifyIntegration(),
-  // output: "server",
+  // output: 'server',
   vite: {
-    appType: "spa",
-    // plugins: [
-    //   TanStackRouterVite(),
-    // ],
     ssr: {
       noExternal: ["react-icons", "@mui/material", "gsap"]
-    },
-  },
+    }
+  }
 });
-// 
